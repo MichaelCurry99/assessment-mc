508307332e62783d255e01f613159a6d0e54789d
Kevanthony <Michael_Curry99@outlook.com>
2022-06-30 20:40:17 -0500

    working on letters

--- README2.md
+++ README2.md
@@ -1,4 +1,4 @@
-  ## Color Flipper- Wednseday, June, 29,2022 - 2:34pm
+  ## Color Flipper- Wednseday, June 29,2022 - 2:34pm
   - What progress have you made for your project so far?
   I made it far enough to when I can display my HTML and make a colorful CSS.
 - Are there any challenges you have / have faced? How will you / did you overcome them?
@@ -7,3 +7,14 @@
   No questions.
 - Are there any concepts you are not so familiar with?
      Everything seems ok right now. 
+
+  ## Color Flipper- Thursday, June 30, 2022 - 1:34pm
+  - What progress have you made for your project so far?
+     I have finally found the way to get the colors to show up. Looking through the web, I found the key word "style.background"
+     to implement the different background that. 
+- Are there any challenges you have / have faced? How will you / did you overcome them?
+  Nothing now.
+- Are there any other questions you have for your instructors?
+  I don't think I have anything on my mind for now.
+- Are there any concepts you are not so familiar with?
+     I'm familiar with the meaning of some of the things in JavaScript. 
--- color flipper/black.js
+++ color flipper/black.js
@@ -1,28 +1,32 @@
 // let firstClick = document.querySelector("#button");
 // let background1 = document.querySelector("body");
 // let background = document.querySelector("body")
-let button = document.getElementById('button');
-// firstClick.addEventListener ("click", () => {
+let button = document.getElementById('button');         // <--   // grabbing the id from html from the tags button
+// firstClick.addEventListener ("click", () => {       // <--  // Trial and error
 //     // background1.classList.remove("yellow");
 //     background1.classList.add("blue"),("yellow");
 //     background2.classList.replace("violet");
    
 // }
 // )
-let color = ["blue","yellow","violet"];
+let color = ["blue","yellow","violet","black"];    // <--  // This will display the colors when using the array
+// let word = ["blue","yellow","violet"];     // <--  // Trial and error
 // let color = ["blue","yellow","violet;"];
 
 // for(i=0; i < color.length;i++){
 //    console.log(color);
 // }
 
-button.addEventListener('click', function(){
+button.addEventListener('click', function(){                     // <--     // I use the function so that I can randomize the colors in the previous array
 
-   var randomColor = color[Math.floor(Math.random() * color.length)]
+   let randomColor = color[Math.floor(Math.random() * color.length)]     // <--    // I declare my variable with let randomColor and use color to randomize the color array.
+   // let randomWord = word[Math.floor(Math.random() * word.length)]     // <--  // Trial and error. Was trying to display the name o
 
-let cody = document.getElementById('body');
+let body = document.getElementById('body');   // <-- // Using my element id, I can create the colors I want in the background
+// let word = document.getElementById('random-word');
 
-body.style.background = randomColor;
+body.style.background = randomColor;  // <-- // The colors will display using randomColor for the entire background. 
+// word.style.random = randomWord;
 })
 
 
